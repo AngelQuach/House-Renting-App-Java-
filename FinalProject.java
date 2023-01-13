a436c47c1a624430a70e09379ec8ef1e8bf42ae4
AngelQuach <122626373+AngelQuach@users.noreply.github.com>
2023-01-13 16:18:58 -0500

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -1,19 +1,5 @@
+//stored in a package named SwingApplication in Eclipse.
 package SwingApplication;
-
-/*
- * This app is a House Renting App. There are three identities that users can choose from House owner, customer, and administrator. This application allows 
- * 1 owner to register and input information about their houses. Once logged out, the owner cannot access info about their houses, but the input houses info 
- * is still available for customers. For customers, there are at most 5 chances for them to register and view info about houses. There is no limitation on 
- * the number of times they can rent houses, but similarly, once they log out, they would have to re-register to view info about houses. Once a house is 
- * rented out, info about it would be cleared and cannot be accessed anymore. Customers can haggle with the owner but they can only get a discount if their 
- * desirable discount is less than or equal to the one provided by the owner. The administrator can view a summary of information about the owner and the 
- * houses.
- * 
- * The picture is from: https://www.publicdomainpictures.net/pictures/360000/velka/blue-sky-background-with-clouds-1596445447aMB.jpg
- * 
- * Author: Sze Ying (Angel) Quach 
- * Date: May 28, 2022
- */
 
 import java.awt.*;
 import java.awt.event.*;
